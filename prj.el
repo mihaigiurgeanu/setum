934858b95daf699848926669376c347e0f5bd4dc
mihai <mihai>
2005-11-28 20:16:49 +0000

    Adaugat prj.el la proiect.

---
+++ prj.el
@@ -0,0 +1,8 @@
+(jde-project-file-version "1.0")
+(jde-set-variables
+ '(jde-global-classpath (quote ("C:/JOnAS-4.4.3/lib/client.jar" "C:/JOnAS-4.4.3/lib/commons/j2ee/ejb-2_1-api.jar" "C:/JOnAS-4.4.3/lib/commons/j2ee/servlet-2_4.jar" "C:/JOnAS-4.4.3/lib/commons/j2ee/jsp-2_0.jar" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/classes/" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/java/" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/lib/jasperreports-0.6.8.jar" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/lib/commons-cli-1.0.jar" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/lib/jacl.jar" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/lib/tcljava.jar" "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/lib/commons-beanutils-1.5.jar")))
+ '(jde-compiler (quote ("javac" "")))
+ '(jde-compile-option-directory "c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/classes")
+ '(jde-ant-home "C:\\JOnAS-4.4.3\\ant")
+ '(jde-compile-option-sourcepath (quote ("c:/Documents and Settings/Mihai Giurgeanu/My Documents/src/setum/java/")))
+ '(jde-ant-invocation-method (quote ("Ant Server"))))
