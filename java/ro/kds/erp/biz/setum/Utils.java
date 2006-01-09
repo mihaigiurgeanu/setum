6062c819088831ec61ceb6728e2843005bd52b89
mihai <mihai>
2006-01-09 12:04:50 +0000

    Clasa care contine diverse metode statice folosite in diverse parti ale proiectului. Nu are nici o metoda acum.

---
+++ java/ro/kds/erp/biz/setum/Utils.java
@@ -0,0 +1,17 @@
+package ro.kds.erp.biz.setum;
+
+
+
+
+/**
+ * Static methods for supporting the business logic.
+ *
+ *
+ * Created: Wed Dec 14 00:21:47 2005
+ *
+ * @author <a href="mailto:mihai@cris.kds.ro">Mihai Giurgeanu</a>
+ * @version 1.0
+ */
+public class Utils {
+    
+} // Utils
