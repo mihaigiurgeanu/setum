063a4a22e494b347edc5af6d8d87e323f001e0b3
mihai <mihai>
2006-03-25 21:16:19 +0000

    Adaugat categorie Maner/Semicilindru exterior

--- java/ro/kds/erp/web/DataInit.java
+++ java/ro/kds/erp/web/DataInit.java
@@ -49,84 +49,48 @@ public class DataInit extends HttpServlet {
 	    // the objects with ids lower then 10000 are considered system
 	    // objects, and they have "known values" through the application
 	    CategoryLocal sistemeCat = createCategory(9999, "sisteme");
-// 	    Collection sisteme = sistemeCat.getSubCategories();
 	    addSub(sistemeCat, 9998, "Broasca");
 	    addSub(sistemeCat, 9997, "Cilindru");
-// 	    sisteme.add(ch.create(new Integer(9996), "Sild"));
 	    addSub(sistemeCat, 9996, "Sild");
-// 	    sisteme.add(ch.create(new Integer(9995), "Yalla"));
 	    addSub(sistemeCat, 9995, "Yalla");
-// 	    ch.create(new Integer(9994), "Vizor");
 	    createCategory(9994, "Vizor");
-// 	    ch.create(new Integer(9993), "Usa standard");
 	    createCategory(9993, "Usa standard");
-// 	    ch.create(new Integer(9992), "Usa simpla");
 	    createCategory(9992, "Usa simpla");
-// 	    ch.create(new Integer(9991), "Usa metalica cu 1 canat");
-//	    createCategory(9991, "Usa metalica cu 1 canat");
-// 	    ch.create(new Integer(9990), "Usa metalica cu 2 canate");
 	    createCategory(9990, "Usa metalica");
-// 	    ch.create(new Integer(9989), "Fereastra");
 	    createCategory(9989, "Fereastra");
 
-// 	    CategoryLocal othersCat = ch.create(new Integer(9988), 
-// 						"Diverse");
 	    CategoryLocal othersCat = createCategory(9988, "Diverse");
-// 	    Collection others = othersCat.getSubCategories();
-// 	    others.add(ch.create(new Integer(9987), "Geam simmplu"));
 	    addSub(othersCat, 9987, "Gean simplu");
-// 	    others.add(ch.create(new Integer(9986), "Geam termopan"));
 	    addSub(othersCat, 9986, "Gean termopan");
-// 	    others.add(ch.create(new Integer(9985), "Grilaj"));
 	    addSub(othersCat, 9985, "Grilaj");
-// 	    others.add(ch.create(new Integer(9984), "Tabla"));
 	    addSub(othersCat, 9984, "Tabla");
 
-// 	    ch.create(new Integer(9983), "Grila ventilatie");
 	    createCategory(9983, "Grila ventilatie");
-// 	    ch.create(new Integer(9982), "Gauri aerisire");
 	    createCategory(9982, "Gauri aerisire");
-// 	    ch.create(new Integer(9981), "Supralumina");
 	    createCategory(9981, "Supralumina");
-// 	    ch.create(new Integer(9980), "Panou lateral");
 	    createCategory(9980, "Panou lateral");
-// 	    ch.create(new Integer(9979), "Ghiseu");
 	    createCategory(9979, "Ghiseu");
 
-// 	    others.add(ch.create(new Integer(9978), "Pret grila ventilatie"));
 	    addSub(othersCat, 9978, "Pret grila ventilatie");
 
-// 	    sisteme.add(ch.create(new Integer(9977), "Maner"));
 	    addSub(sistemeCat, 9977, "Maner");
-// 	    sisteme.add(ch.create(new Integer(9976), "Bara antipanica"));
 	    addSub(sistemeCat, 9976, "Bara antipanica");
-// 	    sisteme.add(ch.create(new Integer(9975), "Selector ordine"));
 	    addSub(sistemeCat, 9975, "Selector ordine");
-// 	    sisteme.add(ch.create(new Integer(9974), "Decupare sistem"));
 	    addSub(sistemeCat, 9974, "Decupare sistem");
 
 
 
-// 	    CategoryLocal valueListsCategory = ch.create(new Integer(9973), "Liste de valori");
 	    CategoryLocal valueListsCategory = createCategory(9973, "Liste de valori");
 
 
-// 	    // the objects with ids over 10000 are user objects;
-// 	    // create here some for convenience
-// 	    sisteme.add(ch.create(new Integer(10000), "Cheie"));
 	    addSub(sistemeCat, 10000, "Cheie");
-// 	    sisteme.add(ch.create(new Integer(10001), "Amortizor"));
 	    addSub(sistemeCat, 10001, "Amortizor");
-// 	    sisteme.add(ch.create(new Integer(10002), "Bara de protectie"));
 	    addSub(sistemeCat, 10002, "BaraProtectie");
-// 	    sisteme.add(ch.create(new Integer(10003), "Lant de siguranta"));
 	    addSub(sistemeCat, 10003, "Lant de siguranta");
-// 	    sisteme.add(ch.create(new Integer(10004), "Rozeta"));
 	    addSub(sistemeCat, 10004, "Rozeta");
-// 	    sisteme.add(ch.create(new Integer(10005), "Protectie"));
 	    addSub(sistemeCat, 10005, "Protectie");
-// 	    sisteme.add(ch.create(new Integer(10006), "Copiat cheie"));
 	    addSub(sistemeCat, 10006, "Copiat cheie");
+	    addSub(sistemeCat, 10007, "Maner/Semicilindru exterior");
 
 
 	} catch (Exception e) {
