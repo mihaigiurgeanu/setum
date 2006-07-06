cda3602d34f6477d8944dc90e048b6b5087eee97
mihai <mihai>
2006-07-06 07:20:33 +0000

    calcul camp descriere (folosesc codurile ... trebuie transformat pentru a folosi numele produselor).

---
+++ scripting/Finisaje_calculatedFields.tcl
@@ -0,0 +1,26 @@
+# Finisaje_calculatedFields.tcl
+
+set description "Finisaje:"
+
+if { $zincare == 1 } {
+    set description "$description Zincare"
+}
+if { $capitonare == 1 } {
+    set description "$description Capitonare"
+}
+if { $placare == 1 } {
+    set description "$description Placare"
+}
+if { $grundId != 0 } {
+    set description "$description Grund $grundId"
+}
+if { $vopsireTip == 1 } {
+    set description "$description Vopsire normala"
+}
+if { $vopsireTip == 2 } {
+    set description "$description Vopsire VCE"
+}
+if { $ralStasId != 0 } {
+    set description "$description RAL STAS $ralStasId"
+}
+
