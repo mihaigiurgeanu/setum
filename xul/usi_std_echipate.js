2e27728361b6265a0121ce6133d3a969b7a7b588
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2010-05-18 20:53:10 +0300

    Listarea usilor standard nu mai schimba tab-ul.
    La listarea usilor standard, apelam procedura locala addnew() care
    modifica implicit tab-ul curent. Am impartit procedura in doua parti,
    una care trimite comanda de addnew la server si una care schimba tab-ul.
    
    La initializarea paginii, apelez doar partea de trimitere a comenzii la
    server.

--- xul/usi_std_echipate.js
+++ xul/usi_std_echipate.js
@@ -32,12 +32,15 @@ function save() {
     document.getElementById('maintab').selectedIndex=0;
 }
 
-function addnew() {
+function send_new_command() {
+  var req = theForm.get_request();
+  req.add("command", "newFormData");
+  theForm.post_request(req);
+}
 
-    var req = theForm.get_request();
-    req.add("command", "newFormData");
-    theForm.post_request(req);
-    document.getElementById('maintab').selectedIndex=1;
+function addnew() {
+  send_new_command();
+  document.getElementById('maintab').selectedIndex=1;
 }
 
 function remove_selected_product() {
@@ -50,6 +53,5 @@ function remove_selected_product() {
 theForm.setupEventListeners();
 
 // addnew pentru select filtru de selectie
-theForm.addnew();
-document.getElementById('maintab').selectedIndex=0;
+send_new_command();
 load_usi();
