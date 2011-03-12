8445c100271846cd0fd1aa680676e1fdd4bde255
Mihai Giurgeanu <mihai.giurgeanu@genesissoftware.ro>
2011-03-12 10:57:45 +0200

    Copiat fisierele xul in distributie.

--- distrib/setum/chrome/xul/orders.js
+++ distrib/setum/chrome/xul/orders.js
@@ -323,11 +323,24 @@ function on_new_invoice() {
   clear_payments();
 }
 
+function on_new_invoice_from_proforma() {
+  var req = theForm.get_request();
+  req.add("command", "newInvoiceFromProforma");
+  theForm.post_request(req);
+  maintab.selectedIndex = 6;
+}
+
 function on_new_payment() {
-    var req = theForm.get_request();
-    req.add("command", "newPaymentData");
-    theForm.post_request(req);
-    maintab.selectedIndex = 7;
+  // salvez factura
+  var req = theForm.get_request();
+  req.add("command", "saveInvoiceData");
+  theForm.post_request(req);
+
+  // adaug noua plata
+  req = theForm.get_request();
+  req.add("command", "newPaymentData");
+  theForm.post_request(req);
+  maintab.selectedIndex = 7;
 }
 
 function on_save_invoice() {
@@ -341,13 +354,14 @@ function on_save_invoice() {
 }
 
 function on_save_payment() {
-    var req = theForm.get_request();
-    req.add("command", "savePaymentData");
-    theForm.post_request(req);
-    load_selected_order();
-    load_selected_invoice();
-    load_payments();
-    maintab.selectedIndex = 6;
+  // salvez plata
+  req = theForm.get_request();
+  req.add("command", "savePaymentData");
+  theForm.post_request(req);
+  load_selected_order();
+  load_selected_invoice();
+  load_payments();
+  maintab.selectedIndex = 6;
 }
 
 function on_remove_invoice() {
